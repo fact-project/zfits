c7d127d68594182af6ec229aa745eb407a574a1c
Dominik Neise <neised@phys.ethz.ch>
2016-08-23 08:08:16 +0200

    fix bug related to c-division and reverting the preconditioning

--- zfits/cython_tools.pyx
+++ zfits/cython_tools.pyx
@@ -6,9 +6,10 @@ ctypedef np.int16_t DTYPE_t
 cimport cython
 @cython.boundscheck(False) # turn off bounds-checking for entire function
 @cython.wraparound(False)  # turn off negative index wrapping for entire function
+@cython.cdivision(True)
 def revert_preconditioning_inner(np.ndarray[DTYPE_t, ndim=1] d):
     cdef int N = d.shape[0]
     cdef int i
     for i in range(2, N):
-        d[i] += int((d[i-1] + d[i-2])/2)
+        d[i] += (d[i-1] + d[i-2]) / 2
     return d
