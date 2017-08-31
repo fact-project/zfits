ded0fadc076d6516406de5b07a3de7636f66c656
Dominik Neise <neised@phys.ethz.ch>
2017-08-31 19:36:45 +0200

    we don't need that anymore

--- zfits/fact++wrapper.cpp
+++ zfits/fact++wrapper.cpp
@@ -1,31 +1,8 @@
-/*
-c_multiply.c
-
-simple C function that alters data passed in via a pointer
-
-    used to see how we can do this with Cython/numpy
-
-*/
-
 #include "huffman.h"
 #include "DrsCalib.h"
 #include "factfits.h"
 
 extern "C"{
-    void c_multiply (double* array, double multiplier, int m, int n) {
-
-        int i, j ;
-        int index = 0 ;
-
-        for (i = 0; i < m; i++) {
-            for (j = 0; j < n; j++) {
-                array[index] = array[index]  * multiplier ;
-                index ++ ;
-                }
-            }
-        return ;
-    }
-
     void Decode_dom(const uint8_t *bufin, size_t bufinlen, int16_t *bufout, size_t bufoutlen) {
         int64_t i = 0;
         const Huffman::Decoder decoder(bufin, i);
