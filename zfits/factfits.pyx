e16059655547851992d6725b09a2f134aa4f7b0f
Dominik Neise <neised@phys.ethz.ch>
2017-08-31 19:36:08 +0200

    we don't need that

--- zfits/factfits.pyx
+++ zfits/factfits.pyx
@@ -157,8 +157,8 @@ class AnotherFoo:
 DTYPE = np.int16
 ctypedef np.int16_t DTYPE_t
 cimport cython
-@cython.boundscheck(False) # turn off bounds-checking for entire function
-@cython.wraparound(False)  # turn off negative index wrapping for entire function
+@cython.boundscheck(False)
+@cython.wraparound(False)
 @cython.cdivision(True)
 def revert_preconditioning_inner(np.ndarray[DTYPE_t, ndim=1] d):
     cdef int N = d.shape[0]
@@ -166,31 +166,6 @@ def revert_preconditioning_inner(np.ndarray[DTYPE_t, ndim=1] d):
     for i in range(2, N):
         d[i] += (d[i-1] + d[i-2]) / 2
     return d
-
-# declare the interface to the C code
-
-cdef extern void c_multiply (double* array, double value, int m, int n)
-
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-def multiply(np.ndarray[double, ndim=2, mode="c"] input not None, double value):
-    """
-    multiply (arr, value)
-
-    Takes a numpy arry as input, and multiplies each elemetn by value, in place
-
-    param: array -- a 2-d numpy array of np.float64
-    param: value -- a number that will be multiplied by each element in the array
-
-    """
-    cdef int m, n
-
-    m, n = input.shape[0], input.shape[1]
-
-    c_multiply (&input[0,0], value, m, n)
-
-    return None
 
 cdef extern void Decode_dom (const np.uint8_t *bufin, size_t bufinlen, np.int16_t *bufout, size_t bufoutlen)
 
