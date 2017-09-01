8f1e49ad5e469eb6b267499fb314507fb7292dd7
Dominik Neise <neised@phys.ethz.ch>
2017-09-01 11:00:19 +0200

    adjust test and compare more than just pixel 0 ...

--- tests/test_zfits_calib.py
+++ tests/test_zfits_calib.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def test_with_fact_tools():
-    from zfits import FactFits
+    from zfits import FactFitsCalib
 
     fact_tools_reference = fitsio.read(
         'tests/resources/20160817_016_calibrated.fits'
@@ -11,11 +11,20 @@ def test_with_fact_tools():
 
     print(fact_tools_reference.shape)
 
-    f = FactFits(
+    f = FactFitsCalib(
         'tests/resources/20160817_016.fits.fz',
         'tests/resources/20160817_030.drs.fits.gz',
     )
-
-    assert np.all(np.isclose(
-        f.get_data_calibrated(0)[0, 10:250], fact_tools_reference[0, 10:250]
-    ))
+    event = next(f)
+    # in pixel 18 is a spike:
+    #  - in fact_tools_reference, this spike is not removed
+    #  - in the FactFitsCalib['CalibData'] the spike **is removed**.
+    # so the test would fail ...
+    for i in range(17):
+        a = event['CalibData'][i, 10:250]
+        b = fact_tools_reference[i, 10:250]
+        d = a - b
+        dabs = np.abs(d)
+        argmax = np.argmax(dabs)
+        print(i, argmax, dabs[argmax], d[argmax], a[argmax], b[argmax])
+        assert np.all(np.isclose(a, b))
