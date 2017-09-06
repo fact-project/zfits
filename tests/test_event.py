7df08637a86f92ef55f5e13a52b383ba004a2195
Dominik Neise <neised@phys.ethz.ch>
2017-09-06 17:50:00 +0200

    more tests

--- tests/test_event.py
+++ tests/test_event.py
@@ -11,13 +11,19 @@ def test_event():
 
 def test_event_shapes():
     from zfits import FactFits
+    from zfits import FactFitsCalib
 
-    f = FactFits('tests/resources/20160817_016.fits.fz')
-    event = next(f)
+    for file in [FactFitsCalib(
+        'tests/resources/testDataFile.fits.gz',
+        'tests/resources/testDrsFile.drs.fits.gz',
+        ),
+        FactFits('tests/resources/20160817_016.fits.fz')
+        ]:
+        event = next(file)
 
-    for k, v in event.items():
-        print(k, v)
-        if v.ndim == 1:
-            assert v.shape[0] != 1
-        if k in ['EventNum', 'NumBoards', 'TriggerNum', 'TriggerType']:
-            assert v.ndim == 0
+        for k, v in event.items():
+            print(k, v)
+            if v.ndim == 1:
+                assert v.shape[0] != 1
+            if k in ['EventNum', 'NumBoards', 'TriggerNum', 'TriggerType']:
+                assert v.ndim == 0
